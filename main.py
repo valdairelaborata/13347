c8dd471216bfb874998169f6c48e45ab3cfd7ca7
Valdair <valdairelaborata@gmail.com>
2025-08-20 19:19:19 -0300

    heranca

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ class CarroEsportivo(Carro):
     def descricao(self):
         return super().descricao() + f" {self.velcidade_maxima}"
 
+class CarroSedan(Carro):
+    def __init__(self, marca, modelo, ano, tamanho_porta_malas):
+        super().__init__(marca, modelo, ano)
+        self.tamanho_porta_malas = tamanho_porta_malas
+
+    def descricao(self):
+        return super().descricao() + f" Porta malas {self.tamanho_porta_malas}"         
 
 
 carro = Carro("Ford", "KA", "2008")
@@ -23,6 +30,9 @@ print(carro.descricao())
 
 esportivo = CarroEsportivo("Honda", "Civic TSI", "2008", 240)
 print(esportivo.descricao())
+
+sedan = CarroSedan("Fiat", "Sienna", 2013, 450)
+print(sedan.descricao())
 
 
 
