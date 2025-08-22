6df176dfe9a816f86f945c46526f183f30f055d5
Valdair <valdairelaborata@gmail.com>
2025-08-21 21:18:54 -0300

    decoradores

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
-class Carro:
-    def __init__(self, marca, modelo, ano):
-        self.marca = marca
-        self.modelo = modelo
-        self.ano = ano
-
-    # def __del__(self):
-    #     print(f"Carro {self.modelo} sendo destruído")
-
-    def __str__(self):
-        return f"{self.marca} - {self.modelo} - {self.ano}"
 
 
-    def __repr__(self):
-        return f"Carro(marca: {self.marca}, modelo: {self.modelo})"
+def aplicar(funcao, valor):
+    return funcao(valor)
 
-    def __eq__(self, outro_objeto):
-        return self.marca == outro_objeto.marca and self.modelo == outro_objeto.modelo
-    
-    def __ne__(self, outro_objeto):
-        return not self.__eq__(outro_objeto)
+def dobrar(numero):
+    return numero * 2
 
+def triplicar(numero):
+    return numero * 3
 
-# del carro
+resultado = aplicar(dobrar, 5)
+print(resultado)
+resultado = aplicar(triplicar, 5)
+print(resultado)
 
-# print(carro)
-
-# print(repr(carro))  
-  
-
-carro_01 = Carro("Ford", "KA", 2008)
-carro_02 = Carro("Honda", "Civic", 2012)
-
-
-# sao_iguais = carro_01 == carro_02
-
-sao_diferentes = carro_01 != carro_02
-
-print(sao_diferentes)
-
-
-
-
-print("Fim")
