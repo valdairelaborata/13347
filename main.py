f5415a836e890993b1f9b03c876e713ddf320436
Valdair <valdairelaborata@gmail.com>
2025-08-27 19:44:08 -0300

    fast_api

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def teste():
-    return {"mensagem": "Teste de resquest no método GET"}
+@app.get("/usuario")
+def listar_usuarios():
+    return {"mensagem": "Dados de usuário"}
+
+@app.get("/usuario/{id}")
+def obter_usuario(id: int):
+    return {"mensagem": f"Usuário {id}"}
+
+@app.get("/usuario/cpf/{cpf}")
+def obter_usuario_por_cpf(cpf: str):
+    return {"mensagem": f"Usuário localizado com o cpf: {cpf}"}
+
+
+@app.get("/opa")
+def teste02():
+    return {"mensagem": "Opa!"}
+
